2b65f65ca6a96c7abc9a2305dc0a4f347e41837a
Tomas Herrera <tomasherrera91@hotmail.com>
2015-04-22 01:41:49 -0500

    Added orders

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -14,8 +14,15 @@
 
   app.controller("HomeController", function($scope, $resource, $routeParams, $location) {
     var AllProducts = $resource("/products/index.json", {}, {});
-    $scope.tab = 1;
+    $scope.tab = 2;
     $scope.selected_product = null;
+    $scope.selected_brand = "";
+    $scope.selected_presentation = "";
+    $scope.presentation_selected = false;
+    $scope.amount_value = 1;
+    $scope.amount = 1;
+    $scope.total = 0;
+    $scope.ordeable = true;
 
     $scope.getAllProducts = function(){
       AllProducts.get().$promise.then(
@@ -37,6 +44,30 @@
         $scope.dashboard_style = "";
       };
     };
+
+    $scope.selectedBrandAndPresentation = function(brand, presentation){
+      $scope.selected_brand = brand;
+      $scope.selected_presentation = presentation;
+      $scope.presentation_selected = true;
+    };
+
+    $scope.order_items = [
+      {}
+    ];
+
+    $scope.addProductToOrder = function(selected_product, amount_chosen, selected_brand, selected_presentation){
+      var order_item = {
+        serial: selected_product.serial,
+        title: selected_product.title,
+        amount: amount_chosen,
+        brand: selected_brand,
+        presentation: selected_presentation,
+        price: selected_product.title.length * 1000 * amount_chosen
+      };
+      $scope.order_items.push(order_item);
+      $scope.total = parseInt($scope.total) + parseInt(order_item.price);
+
+    }
 
     $scope.changeView = function(view){
       $location.path(view);
